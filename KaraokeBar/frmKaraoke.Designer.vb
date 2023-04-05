1f46b1d157e64283c1483ab1ab082fbb52ec6f11
K-c00per <katelyn.cooper@tntns.com>
2023-04-04 23:26:53 -0400

    Add project files.

---
+++ KaraokeBar/frmKaraoke.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmKaraoke
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmKaraoke))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.cboDropDown = New System.Windows.Forms.ComboBox()
+        Me.lblRental = New System.Windows.Forms.Label()
+        Me.txtValue = New System.Windows.Forms.TextBox()
+        Me.btnTotalCost = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lblTotalCost = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(379, 502)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.White
+        Me.lblTitle.Location = New System.Drawing.Point(485, 31)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(332, 37)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "Karaoke Music Night"
+        '
+        'cboDropDown
+        '
+        Me.cboDropDown.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboDropDown.FormattingEnabled = True
+        Me.cboDropDown.Items.AddRange(New Object() {"Number of Songs ($2.99 per song)", "Hours in Private Karaoke Booth ($8.99 per hour)"})
+        Me.cboDropDown.Location = New System.Drawing.Point(419, 98)
+        Me.cboDropDown.Name = "cboDropDown"
+        Me.cboDropDown.Size = New System.Drawing.Size(453, 28)
+        Me.cboDropDown.TabIndex = 2
+        '
+        'lblRental
+        '
+        Me.lblRental.AutoSize = True
+        Me.lblRental.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRental.ForeColor = System.Drawing.Color.White
+        Me.lblRental.Location = New System.Drawing.Point(476, 188)
+        Me.lblRental.Name = "lblRental"
+        Me.lblRental.Size = New System.Drawing.Size(350, 25)
+        Me.lblRental.TabIndex = 3
+        Me.lblRental.Text = "Hourly Rental of Karaoke Room:"
+        Me.lblRental.Visible = False
+        '
+        'txtValue
+        '
+        Me.txtValue.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtValue.Location = New System.Drawing.Point(617, 260)
+        Me.txtValue.Name = "txtValue"
+        Me.txtValue.Size = New System.Drawing.Size(53, 26)
+        Me.txtValue.TabIndex = 4
+        Me.txtValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.txtValue.Visible = False
+        '
+        'btnTotalCost
+        '
+        Me.btnTotalCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTotalCost.Location = New System.Drawing.Point(470, 344)
+        Me.btnTotalCost.Name = "btnTotalCost"
+        Me.btnTotalCost.Size = New System.Drawing.Size(146, 42)
+        Me.btnTotalCost.TabIndex = 5
+        Me.btnTotalCost.Text = "Total Cost"
+        Me.btnTotalCost.UseVisualStyleBackColor = True
+        Me.btnTotalCost.Visible = False
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(677, 344)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(146, 42)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear Form"
+        Me.btnClear.UseVisualStyleBackColor = True
+        Me.btnClear.Visible = False
+        '
+        'lblTotalCost
+        '
+        Me.lblTotalCost.AutoSize = True
+        Me.lblTotalCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalCost.ForeColor = System.Drawing.Color.White
+        Me.lblTotalCost.Location = New System.Drawing.Point(507, 420)
+        Me.lblTotalCost.Name = "lblTotalCost"
+        Me.lblTotalCost.Size = New System.Drawing.Size(276, 24)
+        Me.lblTotalCost.TabIndex = 7
+        Me.lblTotalCost.Text = "Total Cost of Karaoke Night -"
+        Me.lblTotalCost.Visible = False
+        '
+        'frmKaraoke
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(905, 504)
+        Me.Controls.Add(Me.lblTotalCost)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnTotalCost)
+        Me.Controls.Add(Me.txtValue)
+        Me.Controls.Add(Me.lblRental)
+        Me.Controls.Add(Me.cboDropDown)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "frmKaraoke"
+        Me.Text = "Karaoke Music Night"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents cboDropDown As ComboBox
+    Friend WithEvents lblRental As Label
+    Friend WithEvents txtValue As TextBox
+    Friend WithEvents btnTotalCost As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lblTotalCost As Label
+End Class
---
+++ KaraokeBar/frmKaraoke.vb
@@ -0,0 +1,114 @@
+﻿' Program Name: Karaoke Music Night
+' Developers: Katelyn Cooper
+' Date: April 4, 2023
+' Purpose: This application displays the cost of renting a Karaoke room based on the song selection and how many hours rented.
+Option Strict On
+
+Public Class frmKaraoke
+
+    Private _decSong As Decimal = 2.99D
+    Private _decHourlyRate As Decimal = 8.99D
+    Private Sub Karaoke_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Hold the splash screen for about 6 seconds
+        Threading.Thread.Sleep(3000)
+    End Sub
+    Private Function ValidateInput() As Boolean
+        'This procedure validates the input
+        Dim intNumber As Integer
+        Dim blnValid As Boolean = False
+        Try
+            'converts the target amount to an integer value
+            intNumber = Convert.ToInt32(txtValue.Text)
+            If intNumber > 0D Then
+                blnValid = True
+                Return blnValid
+            Else
+                MsgBox("Please enter a number greater than 0", , "Error")
+            End If
+        Catch Exception As FormatException
+            'This catch block detects letters, symbols, blank entries, etc...
+            MsgBox("Please enter a valid amount", , "Error")
+        Catch Exception As OverflowException
+            'This catch block detects numbers that are too large or too small
+            MsgBox("Please enter a reasonable amount", , "Error")
+        Catch Exception As SystemException
+            'This catch block detects a generic exception not caught by the earlier catch blocks
+            MsgBox("Entry invalid. Plese enter a valid number representing the number in your party", , "Error")
+        End Try
+        'Place focus back onto the textbox
+        txtValue.Focus()
+        txtValue.Clear()
+        Return blnValid
+    End Function
+
+    Private Function ComputeSongCost(ByVal intPass As Integer) As Decimal
+        Dim decPassCost As Decimal
+        decPassCost = intPass * _decSong
+        Return decPassCost
+    End Function
+
+    Private Function ComputeRoomCost(ByVal intPass As Integer) As Decimal
+        Dim decTicketCost As Decimal
+        decTicketCost = intPass * _decHourlyRate
+        Return decTicketCost
+    End Function
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'The btnClear click event clears the form
+        ClearForm()
+    End Sub
+
+    Private Sub ClearForm()
+        'Clears the form
+        cboDropDown.SelectedIndex = -1
+        lblRental.Visible = False
+        txtValue.Visible = False
+        btnTotalCost.Visible = False
+        btnClear.Visible = False
+        lblTotalCost.Visible = False
+        txtValue.Clear()
+    End Sub
+
+    Private Sub btnTotalCost_Click(sender As Object, e As EventArgs) Handles btnTotalCost.Click
+        Dim blnAmountIsValid As Boolean = False
+        Dim intValue As Integer
+        Dim decTotal As Decimal
+
+        'A function procedure is called to validate teh value entered
+        blnAmountIsValid = ValidateInput()
+        If blnAmountIsValid = True Then
+            'Input is assigned to varaible 
+            intValue = Convert.ToInt32(txtValue.Text)
+            If cboDropDown.SelectedIndex = 0 Then
+                decTotal = ComputeSongCost(intValue)
+            Else
+                decTotal = ComputeRoomCost(intValue)
+            End If
+
+            lblTotalCost.Visible = True
+            lblTotalCost.Text = "Total Cost of Karaoke Night - " & decTotal.ToString("C")
+        End If
+    End Sub
+
+    Private Sub cboDropDown_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboDropDown.SelectedIndexChanged
+        'The event places the objects on the form
+        If cboDropDown.SelectedIndex = 0 Then
+            lblRental.Text = "Number of Karaoke Songs"
+            lblRental.Visible = True
+            txtValue.Visible = True
+            btnClear.Visible = True
+            btnTotalCost.Visible = True
+            'places focus on the textbox
+            txtValue.Focus()
+        End If
+        If cboDropDown.SelectedIndex = 1 Then
+            lblRental.Text = "Hourly Rental of Karaoke Room:"
+            lblRental.Visible = True
+            txtValue.Visible = True
+            btnTotalCost.Visible = True
+            btnClear.Visible = True
+            'places focus on the textbox
+            txtValue.Focus()
+        End If
+    End Sub
+End Class
